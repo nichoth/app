567d43b1a79389735fd27ae270db569cb25be812
Nick Thomas <nichoth@gmail.com>
2018-02-06 17:14:18 -0800

    first commit

---
+++ example/index.js
@@ -0,0 +1,31 @@
+var Sub = require('../subscription')
+var assert = require('assert')
+var Bus = require('@nichoth/events')
+var Store = require('@nichoth/state')
+
+var DemoStore = Store.extend({
+    _state: { hello: 'world' },
+    foo: function (data) {
+        this._state.hello = data
+    },
+    bar: function (data) {
+        this._state.hello = data
+    }
+})
+
+var bus = Bus()
+var demoStore = DemoStore()
+Sub(demoStore, bus)
+    .on('example', 'foo')
+    .on('woo', demoStore.bar)
+
+bus.emit('example', 'again')
+console.log(demoStore.state().hello)
+assert.equal(demoStore.state().hello, 'again')
+
+bus.emit('woo', 'moo')
+console.log(demoStore.state().hello)
+assert.equal(demoStore.state().hello, 'moo')
+
+
+
